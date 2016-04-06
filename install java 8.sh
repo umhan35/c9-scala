566935980eadef475657610b82059eb33595626d
Zhao Han <cx.chenghai+github@gmail.com>
2016-04-06 18:32:14 -0500

    java 8: auto accept license

--- install java 8.sh
+++ install java 8.sh
@@ -1,4 +1,5 @@
 echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | sudo tee -a /etc/apt/sources.list.d/ppa-webupd8team-java.list
 
 sudo apt-get update
-sudo apt-get -y install oracle-java8-installer
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+sudo apt-get --yes --force-yes install oracle-java8-installer
